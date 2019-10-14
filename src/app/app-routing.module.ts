01e5adc9349d06ceeee701406245c10a31ef2db0
Silicob <Silicob>
2019-10-13 17:41:44 -0700

    updated app-routing.module.ts with 2 new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { SettingsComponent } from './settings/settings.component';
 import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 import { HomeComponent } from './home/home.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { ReportsComponent } from './reports/reports.component';
 
 
 const routes: Routes = [
+  {
+    path: "reports",
+    component: ReportsComponent
+  },
+  {
+    path: "transactionHistory",
+    component: TransactionHistoryComponent
+  },
   {
     path: "settings",
     component: SettingsComponent
