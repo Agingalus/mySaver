da004852e1b0baade8a7fa4dd5c6c439d3974492
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-22 19:46:00 -0700

    Add new transactions panel works

--- src/app/transactionpage-add/transactionpage-add.component.ts
+++ src/app/transactionpage-add/transactionpage-add.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
+export interface historyData {
   position: number;
-  limit: number;
-  balance: number;
+  date: string;
+  account: string;
+  category: string;
+  payee: string;
+  memo: string;
+  amount: number;
+  accountRunningTotal: number;
+  editTransactionOrViewDetails: string;
 }
 
 export interface Food {
@@ -12,10 +17,10 @@ export interface Food {
   viewValue: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Saving', limit: 1.0079, balance: 3454},
-  {position: 2, name: 'Saving', limit: 4.0026, balance: 3445},
-  {position: 3, name: 'Checking', limit: 6.941, balance: 4566},
+const ELEMENT_DATA: historyData[] = [
+  {position: 1, date: '10-22-2019', account: 'checking', category: 'gym', payee: 'BC', memo: '', amount: 58.00, accountRunningTotal: 430.23, editTransactionOrViewDetails: 'button'},
+  {position: 2, date: '10-22-2019', account: 'visa', category: 'books', payee: 'BC', memo: 'Angular', amount: 100.00, accountRunningTotal: 1000.94, editTransactionOrViewDetails: 'button'},
+  {position: 3, date: '10-22-2019', account: 'visa', category: 'education', payee: 'BC', memo: '', amount: 600.00, accountRunningTotal: 400.94, editTransactionOrViewDetails: 'button'},
 ];
 @Component({
   selector: 'app-transactionpage-add',
@@ -27,9 +32,14 @@ export class TransactionPageAddComponent implements OnInit {
   dataSource = ELEMENT_DATA;
 
   foods: Food[] = [
-    {value: '1', viewValue: 'Checking'},
-    {value: '2', viewValue: 'Saving'},
-  
+    {value: '1', viewValue: 'value1'},
+    {value: '2', viewValue: 'value2'},
+    {value: '3', viewValue: 'value3'},
+    {value: '4', viewValue: 'value4'},
+    {value: '5', viewValue: 'value5'},
+    {value: '6', viewValue: 'value6'},
+    {value: '7', viewValue: 'value7'},
+
   ];
   constructor() { }
 
