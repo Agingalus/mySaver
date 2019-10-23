a6ea86457f53c1aa2069ac29d83244bbb4c9da17
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-22 20:13:21 -0700

    basics of transaction history works

--- src/app/transactionpage-history/transactionpage-history.component.ts
+++ src/app/transactionpage-history/transactionpage-history.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
+export interface historyData {
   position: number;
-  limit: string;
-  balance: number;
+  date: string;
+  account: string;
+  category: string;
+  payee: string;
+  memo: string;
+  amount: number;
+  accountRunningTotal: number;
+  editTransactionOrViewDetails: string;
 }
 
 export interface Food {
@@ -11,11 +16,11 @@ export interface Food {
   viewValue: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Education', limit: "Chase Savings", balance: 567},
-  {position: 2, name: 'Rent', limit: "Chase Banking", balance: 3000},
-  {position: 3, name: 'Car Loan', limit: "Chase Banking", balance: 350},
-];
+const HISTORY_DATA: historyData[] = [
+  {position: 1, date: '10-22-2019', account: 'checking', category: 'gym', payee: 'BC', memo: '', amount: 58.00, accountRunningTotal: 430.23, editTransactionOrViewDetails: 'button'},
+  {position: 2, date: '10-22-2019', account: 'visa', category: 'books', payee: 'BC', memo: 'Angular', amount: 100.00, accountRunningTotal: 1000.94, editTransactionOrViewDetails: 'button'},
+  {position: 3, date: '10-22-2019', account: 'visa', category: 'education', payee: 'BC', memo: '', amount: 600.00, accountRunningTotal: 400.94, editTransactionOrViewDetails: 'button'},
+];;
 
 @Component({
   selector: 'app-transactionpage-history',
@@ -23,8 +28,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./transactionpage-history.component.css']
 })
 export class TransactionPageHistoryComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'limit', 'balance'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['position', 'date', 'account', 'category', 'payee', 'memo', 'amount', 'accountRunningTotal', 'editTransactionOrViewDetails'];
+  dataSource = HISTORY_DATA;
 
   foods: Food[] = [
     {value: '1', viewValue: 'Car Loan'},
