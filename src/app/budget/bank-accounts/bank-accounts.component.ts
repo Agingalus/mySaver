8d8f13d0a1c8e36f43554a7000cccdd086f23cd5
karosoos <karosoos1@gmail.com>
2019-11-18 21:37:58 -0800

    working on input

--- src/app/budget/bank-accounts/bank-accounts.component.ts
+++ src/app/budget/bank-accounts/bank-accounts.component.ts
@@ -4,11 +4,8 @@ import { Accounts } from '../../accounts';
 import { AccountService } from '../../service'
 import { Router } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 
-export interface Food {
-  value: string;
-  viewValue: string;
-}
 
 @Component({
   selector: 'app-bank-accounts',
@@ -18,32 +15,53 @@ export interface Food {
 export class BankAccountsComponent implements OnInit {
   displayedColumns: string[] = ['Bank','AvailableBalance', 'Balance', 'Currency' ];
   
-  ourTransactions: Accounts[] 
-  dataSource = new MatTableDataSource<Accounts>(this.ourTransactions);
+  newAccount: Accounts = new Accounts();
+  ourAccounts: Accounts[] 
+  dataSource = new MatTableDataSource<Accounts>(this.ourAccounts);
  
-  getTransactions(): void {
-    this.myTransactionService.getAllAccounts().subscribe((transactionData: Accounts[]) => {
-      this.ourTransactions = transactionData;
-      this.dataSource = new MatTableDataSource<Accounts>(this.ourTransactions);
+  getAccounts(): void {
+    this.myAccountService.getAllAccounts().subscribe((accountData: Accounts[]) => {
+      this.ourAccounts = accountData;
+      this.dataSource = new MatTableDataSource<Accounts>(this.ourAccounts);
       this.dataSource.paginator = this.paginator;
     })
   }
 
-  constructor(private myTransactionService: AccountService, private router: Router) {}
+
+  bank = new FormControl();
+  bankaccountnumber = new FormControl();
+  accounttype = new FormControl();
+  accountid = new FormControl();
+  availablebalance = new FormControl();
+  balance = new FormControl();
+  currency = new FormControl();
+  friendlyname = new FormControl();
+
+  getLastAccountId() {
+
+  }
+
+
+  addNewAccount(){
+
+    this.newAccount.friendlyname = this.friendlyname.value
+    this.newAccount.currency = this.currency.value
+    this.newAccount.balance = this.balance.value
+    this.newAccount.availablebalance = this.availablebalance.value
+    this.newAccount.accountid = this.accountid.value
+    this.newAccount.accounttype = this.accounttype.value
+    this.newAccount.bankaccountnumber = this.bankaccountnumber.value
+    this.newAccount.bank= this.bank.value
+
+  }
+
+  constructor(private myAccountService: AccountService, private router: Router) {}
     @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
     ngOnInit() {
   
-      this.getTransactions();
-      this.dataSource = new MatTableDataSource<Accounts>(this.ourTransactions);
+      this.getAccounts();
+      this.dataSource = new MatTableDataSource<Accounts>(this.ourAccounts);
       this.dataSource.paginator = this.paginator;
     }
-
-
-  foods: Food[] = [
-    {value: '1', viewValue: 'Checking'},
-    {value: '2', viewValue: 'Saving'},
-  
-  ];
-
 
 }
