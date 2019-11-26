e4b836edf6821be44d11eff3268f9c49a4c00336
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-11-26 00:41:46 -0800

    still not getting add to work for test

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { BudgetService } from '../service';
+import { Budgets } from '../budgets';
+import { HttpClient } from '@angular/common/http';
+
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -13,9 +18,9 @@ export interface Food {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Saving', limit: 1.0079, balance: 3454},
-  {position: 2, name: 'Saving', limit: 4.0026, balance: 3445},
-  {position: 3, name: 'Checking', limit: 6.941, balance: 4566},
+  { position: 1, name: 'Saving', limit: 1.0079, balance: 3454 },
+  { position: 2, name: 'Saving', limit: 4.0026, balance: 3445 },
+  { position: 3, name: 'Checking', limit: 6.941, balance: 4566 },
 ];
 
 @Component({
@@ -26,16 +31,25 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class BudgetComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'limit', 'balance'];
   dataSource = ELEMENT_DATA;
+  addNewBudgetForTest() {
+    let newBudget: Budgets = { "_id": "5ddcac37b948963410ebbc53", "BudgetID": 2, "Name": "popcorn", "GoalCategory": 45, "GoalAmount": 0.99, "Description": "Earn popcorn" }
+    console.log("hi from add new budget for test");
+    //console.log(this.myBudgetService)
+    this.myBudgetService.addBudget(newBudget)//.subscribe();
+  };
+
+
 
   foods: Food[] = [
-    {value: '1', viewValue: 'Checking'},
-    {value: '2', viewValue: 'Saving'},
-  
+    { value: '1', viewValue: 'Checking' },
+    { value: '2', viewValue: 'Saving' },
+
   ];
 
-  constructor() { }
+  constructor(private myBudgetService: BudgetService, private router: Router) { }
 
   ngOnInit() {
+
   }
 
 }
